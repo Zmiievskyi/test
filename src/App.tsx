bb8ceb4784fb9be3e8c4536170b6dda3ee93ba5c
Anton Zmiievskyi <anton.zmievsky@gmail.com>
2023-06-24 01:40:19 +0100

    ww

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import { Tree } from "./components/Tree";
+import { SelectList } from "./components/SelectList";
+
+function App() {
+  return (
+    <Container>
+      {/* <Tree /> */}
+      <SelectList/>
+    </Container>
+  );
+}
+
+export default App;
