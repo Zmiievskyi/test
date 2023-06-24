d5a068aea372e58a9bbef346eef206c14d5a9397
Anton Zmiievskyi <anton.zmievsky@gmail.com>
2023-06-24 01:50:40 +0100

    wwdk

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -14,7 +15,11 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+    <BrowserRouter
+    basename="/goit-react-hw-08-phonebook"
+    >
       <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
